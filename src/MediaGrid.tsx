9dfdcee71d90060d418ce127005f800cd38f92a6
Tom Crockett <pelotom@gmail.com>
2021-03-10 13:55:15 -0800

    Finish CSS for media

--- src/MediaGrid.tsx
+++ src/MediaGrid.tsx
@@ -13,62 +13,19 @@ export default function MediaGrid({ media }: MediaGridProps) {
       className={clsx(
         'twetch-renderer__media-grid',
         media.length > 1 && 'twetch-renderer__media-grid--multi',
+        media.length % 2 === 1 && 'twetch-renderer__media-grid--odd',
       )}
     >
       {media.map(({ type, url }, index: number) => (
-        <li
-          key={index}
-          className="twetch-renderer__media-grid-item"
-          style={{
-            width: media.length % 2 && index === 0 ? '100%' : '50%',
-          }}
-        >
+        <li key={index} className="twetch-renderer__media-grid-item">
           {type === 'image' ? (
             media.length > 1 ? (
-              <div
-                style={{
-                  backgroundImage: `url(${url})`,
-                  borderRadius: index < 2 - (media.length % 2) ? '6px 6px 0px 0px' : undefined,
-
-                  // jss7229
-                  width: '100%',
-                  height: '100%',
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundPosition: 'center',
-                }}
-              ></div>
+              <div style={{ backgroundImage: `url(${url})` }} />
             ) : (
-              <img
-                className="jss2143"
-                style={{
-                  borderRadius: index < 2 ? '6px 6px 0px 0px' : undefined,
-
-                  // jss7229
-                  width: '100%',
-                  height: '100%',
-                }}
-                src={url}
-              />
+              <img className="jss2143" src={url} />
             )
           ) : type === 'video' ? (
-            <video
-              playsInline
-              controls
-              loop
-              style={{
-                // jss321
-                width: '100%',
-                height: '100%',
-                margin: '0 auto',
-
-                // jss416
-                display: 'block',
-                borderRadius: 6,
-              }}
-              preload="metadata"
-              src={url}
-            ></video>
+            <video playsInline controls loop preload="metadata" src={url}></video>
           ) : null}
         </li>
       ))}
