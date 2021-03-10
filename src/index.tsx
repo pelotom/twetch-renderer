2b18c5a74a289451baefc84599f395c7aeddf44f
Tom Crockett <pelotom@gmail.com>
2021-03-09 19:05:49 -0800

    Tweaks

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,1 @@
-import React from 'react';
-import Twetch, { TwetchProps } from './Twetch';
-
-export default function TwetchRenderer(props: Omit<TwetchProps, 'quoted'>) {
-  return <Twetch {...props} />;
-}
+export { default, TwetchProps } from './Twetch';
