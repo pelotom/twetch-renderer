44d7f3a266755594f8c3009c61a7c8547f7a3d6b
Tom Crockett <pelotom@gmail.com>
2021-03-10 12:09:49 -0800

    Nicer labels

--- .storybook/stories.tsx
+++ .storybook/stories.tsx
@@ -7,27 +7,27 @@ import '../styles.css';
 const cases: { txid: string; label: string }[] = [
   {
     txid: '221f3249d9c8b97f96c2b526cc56084e3f206c3ae4a5705510000a845261c922',
-    label: 'basic',
+    label: 'Basic',
   },
   {
     txid: '56f11028376146bacc5d9ac2e47cca86ed2e881d7355c8c896874fa4437fa592',
-    label: 'youtube link and quoted twetch',
+    label: 'YouTube link and quoted twetch',
   },
   {
     txid: '94ff4d5f66ef00a1fede2e88b04ffc6c415e0d6acbb3377539f7ce3db42f3b30',
-    label: 'multiple quoted twetches',
+    label: 'Multiple quoted twetches',
   },
   {
     txid: 'ce2a467027a39dce0bd80952d58c596656225491f8f30643fa7a94fc7cebf93b',
-    label: 'non-previewable external URLs',
+    label: 'Non-previewable external URLs',
   },
   {
     txid: '370cbff4f22a9fceedb95b2f6693b55c65b5d8ac3d2218219b25896be08900ac',
-    label: 'simple twitter link',
+    label: 'Simple Twitter link',
   },
   {
     txid: 'c90eeb1801c2fa13b7d4d28b24b0137cde8f384f18d74598d21d170db578f60c',
-    label: 'twitter link with 2 images',
+    label: 'Twitter link with 2 images',
   },
   {
     txid: '332e66c8b6033dbb70d7cfb6263de5b012073bd8126f6cac3494b2b387d34c5b',
@@ -35,15 +35,15 @@ const cases: { txid: string; label: string }[] = [
   },
   {
     txid: 'a4dd17fd4c98aac876147bc3ab3117242b6d6ffffc225f7b41ca96959a99d00a',
-    label: 'linked tweet without twdata',
+    label: 'Linked tweet without twdata',
   },
   {
     txid: '31ff81d7543c44dddd94f0faf16ed6542fd0db01bb625419522e6de74839f0a5',
-    label: 'twitter, youtube, twetch',
+    label: 'Twitter, YouTube, twetch',
   },
   {
     txid: '4e90440f1a7310db6e7c5a40a558decd3357d3dbf0cd97da106343232f64d0d0',
-    label: 'quote of previous',
+    label: 'Quote of previous',
   },
   {
     txid: '68726c26a7daba637a33cce5ba7a2e9ef15e0139ffc671231d822a04acdc66b5',
@@ -59,11 +59,11 @@ const cases: { txid: string; label: string }[] = [
   },
   {
     txid: '47ec7db2ba265ab525dee32daba761a72912db7550d1b8b4e44865d188301ae9',
-    label: 'single image attached',
+    label: 'Single image attached',
   },
   {
     txid: '3ad17c2996dbed056074f7253ab7b3f8a60bc8138e00f7ceaee7ae712fd134ce',
-    label: 'whole post is just an image bitcoinfile',
+    label: 'Whole post is just an image bitcoinfile',
   },
   {
     txid: '3ea715fb2ff489dfe9af246499ea423a644040202c2b3503038a45646abce214',
@@ -71,7 +71,7 @@ const cases: { txid: string; label: string }[] = [
   },
   {
     txid: '6e5de85d98aee7250bcaf9405a39358cdc641d9ea15b6cd9fb5ce7189afd1426',
-    label: 'bitcoinfiles url-attached image',
+    label: 'Bitcoinfiles url-attached image',
   },
   {
     txid: 'c13423212569606c6860a42b26380b0798219127779629f0e71540479783f528',
@@ -79,19 +79,19 @@ const cases: { txid: string; label: string }[] = [
   },
   {
     txid: '51ad1e22e57cefa64536ae1f3fe9d5328c2fe5fabafed114aeddb8fd8eba1a5c',
-    label: 'megapost',
+    label: 'Megapost',
   },
   {
     txid: '5252fc782b49e81e8ff849e52a535bc40ab478050fb6b9302405642290d3e4e5',
-    label: 'video and image',
+    label: 'Video and image',
   },
   {
     txid: '64dec7e262a07b4b4b232305e01a66558f3882e6e0e16d07c821724bdf7bc7f6',
-    label: 'youtube format 1',
+    label: 'YouTube format 1',
   },
   {
     txid: '9c80e00d06ade2e4178f9a3697f8160adadd7131004ec53abd330cea74a57ab4',
-    label: 'youtube format 2',
+    label: 'YouTube format 2',
   },
 ];
 
