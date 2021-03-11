aa61a59e12e53f03fa99c2bc344bd640d4c4353d
Tom Crockett <pelotom@gmail.com>
2021-03-10 16:54:52 -0800

    Don't make whole tweet a link

--- src/TwitterPreview.tsx
+++ src/TwitterPreview.tsx
@@ -11,24 +11,23 @@ export default function TwitterPreview({ twitterData }: TwitterPreviewProps) {
   const { text, media, user, twt_id } = twitterData;
 
   return (
-    <a
-      href={`https://twitter.com/${user.screen_name}/status/${twt_id}`}
-      className="twetch-renderer__twitter-preview"
-    >
+    <div className="twetch-renderer__twitter-preview">
       <div className="twetch-renderer__twitter-preview-media">
         <MediaGrid media={media.map((url: string) => ({ type: 'image', url }))} />
       </div>
       <p className="twetch-renderer__twitter-preview-text">{text}</p>
-      <header>
-        <img src={user.profile_image_url} />
-        <div>
-          <p className="twetch-renderer__twitter-preview-username">
-            {user.name}
-            <span className="twetch-renderer__twitter-preview-handle">@{user.screen_name}</span>
-          </p>
-          <p>twitter.com</p>
-        </div>
-      </header>
-    </a>
+      <a href={`https://twitter.com/${user.screen_name}/status/${twt_id}`}>
+        <header>
+          <img src={user.profile_image_url} />
+          <div>
+            <p className="twetch-renderer__twitter-preview-username">
+              {user.name}
+              <span className="twetch-renderer__twitter-preview-handle">@{user.screen_name}</span>
+            </p>
+            <p>twitter.com</p>
+          </div>
+        </header>
+      </a>
+    </div>
   );
 }
